7bf2a4840bf486e381ee710691c5ba13b6fbf723
mokitlo <lvlasa187@gmail.com>
2026-08-12 19:58:41 +0300

    Update ni4ego2.lua

--- ni4ego2.lua
+++ ni4ego2.lua
@@ -1,3 +1,6 @@
 ID="cd88b44b-cbd8-494e-b5ed-d366272c33ee";
-RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/mokitlo/nothing/refs/heads/main/message.txt"))();
+RUN_AFTER=nul;
 loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
+
+task.wait(3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mokitlo/nothing/refs/heads/main/message.txt"))()
